980c61067251c5233ce0f32980a6fa44a9c75aa8
Joop Laan <joop.laan@gmail.com>
2022-05-28 10:08:25 +0200

    Update landing page with hero pattern

--- patterns/page-landing-hero.php
+++ patterns/page-landing-hero.php
@@ -5,6 +5,14 @@
  * @package Shambhala Twenty Twenty-Two
  */
 
+// Get URL for Page for post setting.
+$news_page_id = get_option( 'page_for_posts' );
+if ( $news_page_id ) {
+	$news_url = get_permalink( $news_page_id );
+} else {
+	$news_url = '#';
+}
+
 return array(
 	'title'      => __( 'Landing page with hero image.', 'shambhala-twentytwentytwo' ),
 	'categories' => array( 'sh-page' ),
@@ -105,16 +113,111 @@ return array(
 <!-- /wp:spacer --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
+
 <!-- wp:spacer {"height":"50px"} -->
 <div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
-<!-- wp:media-text {"mediaLink":"#","mediaType":"image","mediaWidth":56,"verticalAlignment":"center","imageFill":false,"className":"is-style-default"} -->
-<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-center is-style-default" style="grid-template-columns:56% auto"><figure class="wp-block-media-text__media"><img src="' . esc_url( get_stylesheet_directory_uri() . '/assets/images/ikebana-gong.jpg' ) . '" alt="' . esc_html__( 'A meditation gong and an ikebana', 'shambhala-twentytwentytwo' ) . '"/></figure><div class="wp-block-media-text__content"><!-- wp:heading {"style":{"color":{"text":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dblack)"}}} -->
-<h2 class="has-text-color" style="color:var(--wp--preset--color--black)">' . esc_html__( 'A header that sparks interest', 'shambhala-twentytwentytwo' ) . '</h2>
+
+<!-- wp:media-text {"mediaId":2901,"mediaLink":"' . esc_url( get_stylesheet_directory_uri() . '/assets/images/buddha-statue-circle.jpg' ) . '","mediaType":"image","mediaWidth":33,"verticalAlignment":"center","imageFill":false,"className":"rounded-image is-style-rounded-image"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-center rounded-image is-style-rounded-image" style="grid-template-columns:33% auto"><figure class="wp-block-media-text__media"><img src="' . esc_url( get_stylesheet_directory_uri() . '/assets/images/buddha-statue-circle.jpg' ) . '" alt="' . esc_html__( 'Buddha statue', 'shambhala-twentytwentytwo' ) . '" class="wp-image-2901 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:heading {"textAlign":"left","style":{"color":{"text":"#000000"}},"fontSize":"x-large"} -->
+<h2 class="has-text-align-left has-text-color has-x-large-font-size" style="color:#000000">' . esc_html__( 'Shambhala brings together people of all ages and from all walks of life', 'shambhala-twentytwentytwo' ) . '</h2>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph {"className":"has-text-color"} -->
-<p class="has-text-color">' . esc_html__( 'We like to show the picture for illustration and inspiration, lead the visitor into the story, or highlight a certain point.', 'shambhala-twentytwentytwo' ) . '</p>
-<!-- /wp:paragraph --></div></div>
-<!-- /wp:media-text -->',
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Shambhala is based on trust in human dignity. We offer a path of meditation practice and contemplative arts oriented towards modern life.', 'shambhala-twentytwentytwo' ) . '</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"25px"} -->
+<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link" href="#">' . esc_html__( 'Learn more', 'shambhala-twentytwentytwo' ) . '</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:media-text -->
+
+<!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"align":"wide","layout":{"inherit":true}} -->
+<div class="wp-block-group alignwide"><!-- wp:query {"queryId":106,"query":{"perPage":"5","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":""},"displayLayout":{"type":"list"},"align":"wide","className":"is-style-wp-block-query-small-grey","layout":{"inherit":true}} -->
+<div class="wp-block-query alignwide is-style-wp-block-query-small-grey"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"66%"} -->
+<div class="wp-block-column" style="flex-basis:66%"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="has-large-font-size">' . esc_html__( 'Latest news', 'shambhala-twentytwentytwo' ) . '</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"align":"right"} -->
+<p class="has-text-align-right"><a href="' . $news_url . '">' . esc_html__( 'All news', 'shambhala-twentytwentytwo' ) . '</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:post-template -->
+<!-- wp:group {"layout":{"inherit":true}} -->
+<div class="wp-block-group"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"650px"} -->
+<div class="wp-block-column" style="flex-basis:650px"><!-- wp:post-date {"format":"F j, Y","style":{"typography":{"fontStyle":"italic","fontWeight":"400"}},"fontSize":"small"} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":""} -->
+<div class="wp-block-column"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:post-title {"level":3,"isLink":true,"align":"wide","fontSize":"large"} /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
+<!-- /wp:query --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"align":"wide","layout":{"inherit":true}} -->
+<div class="wp-block-group alignwide"><!-- wp:query {"queryId":106,"query":{"perPage":"5","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":""},"displayLayout":{"type":"list"},"align":"wide","className":"is-style-wp-block-query-small-grey","layout":{"inherit":true}} -->
+<div class="wp-block-query alignwide is-style-wp-block-query-small-grey"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"66%"} -->
+<div class="wp-block-column" style="flex-basis:66%"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="has-large-font-size">' . esc_html__( 'Calendar', 'shambhala-twentytwentytwo' ) . '</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"align":"right"} -->
+<p class="has-text-align-right"><a href="#">' . esc_html__( 'All programs', 'shambhala-twentytwentytwo' ) . '</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:post-template -->
+<!-- wp:group {"layout":{"inherit":true}} -->
+<div class="wp-block-group"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"650px"} -->
+<div class="wp-block-column" style="flex-basis:650px"><!-- wp:post-date {"format":"F j, Y","style":{"typography":{"fontStyle":"italic","fontWeight":"400"}},"fontSize":"small"} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":""} -->
+<div class="wp-block-column"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:post-title {"level":3,"isLink":true,"align":"wide","fontSize":"large"} /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
+<!-- /wp:query --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"25px"} -->
+<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->',
 );
