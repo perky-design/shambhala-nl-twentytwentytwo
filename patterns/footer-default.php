4b8c5a37b70b5df2acceb1fd316a43354e887a89
Joop Laan <joop.laan@gmail.com>
2022-05-29 20:15:09 +0200

    Update footer background color

--- patterns/footer-default.php
+++ patterns/footer-default.php
@@ -10,8 +10,8 @@ return array(
 	'categories'    => array( 'sh-footer' ),
 	'blockTypes'    => array( 'core/template-part/footer' ),
 	'viewportWidth' => 1200,
-	'content'       => '<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"spacing":{"padding":{"top":"10vh","bottom":"40px"},"blockGap":"0px"}},"backgroundColor":"foreground","textColor":"background","className":"has-x-small-font-size shambhala-twentytwentytwo-footer","layout":{"inherit":true}} -->
-<div class="wp-block-group alignfull has-x-small-font-size shambhala-twentytwentytwo-footer has-background-color has-foreground-background-color has-text-color has-background has-link-color" style="padding-top:10vh;padding-bottom:40px"><!-- wp:columns {"verticalAlignment":"top","align":"wide","style":{"spacing":{"blockGap":"0px"}}} -->
+	'content'       => '<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"spacing":{"padding":{"top":"10vh","bottom":"40px"},"blockGap":"0px"}},"backgroundColor":"dark-blue","textColor":"background","className":"has-x-small-font-size shambhala-twentytwentytwo-footer","layout":{"inherit":true}} -->
+<div class="wp-block-group alignfull has-x-small-font-size shambhala-twentytwentytwo-footer has-background-color has-dark-blue-background-color has-text-color has-background has-link-color" style="padding-top:10vh;padding-bottom:40px"><!-- wp:columns {"verticalAlignment":"top","align":"wide","style":{"spacing":{"blockGap":"0px"}}} -->
 <div class="wp-block-columns alignwide are-vertically-aligned-top"><!-- wp:column {"verticalAlignment":"top","width":"20%","style":{"spacing":{"padding":{"top":"0px","right":"4px"}}},"className":"is-style-no-margin"} -->
 <div class="wp-block-column is-vertically-aligned-top is-style-no-margin" style="padding-top:0px;padding-right:4px;flex-basis:20%"><!-- wp:heading {"textColor":"background","fontSize":"medium"} -->
 <h2 class="has-background-color has-text-color has-medium-font-size">Meditatie</h2>
