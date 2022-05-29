d31b5efc276e7b0acc2a0c80c5f42f22b0ee374b
Joop Laan <joop.laan@gmail.com>
2022-05-29 09:44:07 +0200

    Add "Meet the team" block pattern

---
+++ patterns/section-team.php
@@ -0,0 +1,216 @@
+<?php
+/**
+ * Meet the Team
+ *
+ * @package Shambhala Twenty Twenty-Two
+ */
+
+return array(
+	'title'      => __( 'Meet the team section', 'shambhala-twentytwentytwo' ),
+	'categories' => array( 'sh-section' ),
+	'content'    => '<!-- wp:group {"align":"wide"} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"fontSize":"x-large"} -->
+<h2 class="has-x-large-font-size">' . esc_html__( 'Board of Directors', 'shambhala-twentytwentytwo' ) . '</h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":3093,"sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default"><img src="' . esc_url( get_stylesheet_directory_uri() . '/assets/images/section-team-388397-500x500.jpg' ) . '" alt="Gary Johnson" class="wp-image-3093"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="has-large-font-size">Gary Johnson</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Director', 'shambhala-twentytwentytwo' ) . '<br><a href="mailto:example@shambhala.org">example@shambhala.org</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"25px"} -->
+<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":3096,"sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default"><img src="' . esc_url( get_stylesheet_directory_uri() . '/assets/images/section-team-211565-500x500.jpg' ) . '" alt="Lauren Morgan" class="wp-image-3096"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="has-large-font-size">Lauren Morgan</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Director', 'shambhala-twentytwentytwo' ) . '<br><a href="mailto:example@shambhala.org">example@shambhala.org</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"25px"} -->
+<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":3099,"sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default"><img src="' . esc_url( get_stylesheet_directory_uri() . '/assets/images/section-team-172564-500x500.jpg' ) . '" alt="Renee Olson" class="wp-image-3099"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="has-large-font-size">Renee Olson</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Director', 'shambhala-twentytwentytwo' ) . '<br><a href="mailto:example@shambhala.org">example@shambhala.org</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"25px"} -->
+<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":3100,"sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default"><img src="' . esc_url( get_stylesheet_directory_uri() . '/assets/images/section-team-326442-500x500.jpg' ) . '" alt="Linda Kelley" class="wp-image-3100"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="has-large-font-size">Linda Kelley</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Director', 'shambhala-twentytwentytwo' ) . '<br><a href="mailto:example@shambhala.org">example@shambhala.org</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"25px"} -->
+<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":3101,"sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default"><img src="' . esc_url( get_stylesheet_directory_uri() . '/assets/images/section-team-3233182-500x500.png' ) . '" alt="John Mullins" class="wp-image-3101"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="has-large-font-size">John Mullins</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Director', 'shambhala-twentytwentytwo' ) . '<br><a href="mailto:example@shambhala.org">example@shambhala.org</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"25px"} -->
+<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":3103,"sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default"><img src="' . esc_url( get_stylesheet_directory_uri() . '/assets/images/section-team-934-500x500.jpg' ) . '" alt="Kristin Conley" class="wp-image-3103"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="has-large-font-size">Kristin Conley</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Director', 'shambhala-twentytwentytwo' ) . '<br><a href="mailto:example@shambhala.org">example@shambhala.org</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"25px"} -->
+<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":3104,"sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default"><img src="' . esc_url( get_stylesheet_directory_uri() . '/assets/images/section-team-3933751-500x500.png' ) . '" alt="Jessica Hale" class="wp-image-3104"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="has-large-font-size">Jessica Hale</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Director', 'shambhala-twentytwentytwo' ) . '<br><a href="mailto:example@shambhala.org">example@shambhala.org</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"25px"} -->
+<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":3105,"sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default"><img src="' . esc_url( get_stylesheet_directory_uri() . '/assets/images/section-team-2330284-500x500.jpg' ) . '" alt="Tiffany Moore" class="wp-image-3105"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="has-large-font-size">Tiffany Moore MD</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Director', 'shambhala-twentytwentytwo' ) . '<br><a href="mailto:example@shambhala.org">example@shambhala.org</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"25px"} -->
+<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":3106,"sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default"><img src="' . esc_url( get_stylesheet_directory_uri() . '/assets/images/section-team-950-500x500.jpg' ) . '" alt="Larry Glass" class="wp-image-3106"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="has-large-font-size">Larry Glass</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Director', 'shambhala-twentytwentytwo' ) . '<br><a href="mailto:example@shambhala.org">example@shambhala.org</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"25px"} -->
+<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":3107,"sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default"><img src="' . esc_url( get_stylesheet_directory_uri() . '/assets/images/section-team-986-500x500.jpg' ) . '" alt="Ian Araújo" class="wp-image-3107"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="has-large-font-size">Ian Araújo</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Secretary', 'shambhala-twentytwentytwo' ) . '<br><a href="mailto:example@shambhala.org">example@shambhala.org</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"25px"} -->
+<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->',
+);
