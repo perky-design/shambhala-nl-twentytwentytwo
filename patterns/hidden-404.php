62cf588621c85a93a33a9e052a4d452962f234cb
Joop Laan <joop.laan@gmail.com>
2022-05-30 06:04:40 +0200

    Update 404 page

--- patterns/hidden-404.php
+++ patterns/hidden-404.php
@@ -8,11 +8,38 @@
 return array(
 	'title'    => __( '404 error content.', 'shambhala-twentytwentytwo' ),
 	'inserter' => false,
-	'content'  => '<!-- wp:heading {"level":1} -->
-				<h1>' . esc_html__( 'Not found, error 404', 'shambhala-twentytwentytwo' ) . '</h1>
-				<!-- /wp:heading -->
-				<!-- wp:paragraph -->
-				<p>' . esc_html__( 'Oops, the page you are looking for does not exist or is no longer available. Everything is still awesome. Just use the search form.', 'shambhala-twentytwentytwo' ) . '</p>
-				<!-- /wp:paragraph -->
-				<!-- wp:search {"width":75,"widthUnit":"%","showLabel":false,"buttonText":"Search"} /-->',
+	'content'  => '<!-- wp:group {"layout":{"inherit":true}} -->
+<div class="wp-block-group">
+<h1 class="alignwide wp-block-post-title">' . esc_html__( 'Page not found', 'shambhala-twentytwentytwo' ) . '</h1>
+<!-- wp:separator {"align":"wide","className":"is-style-wide"} -->
+<hr class="wp-block-separator wp-block-separator-after-title-featured-image alignwide is-style-wide"/>
+<!-- /wp:separator --></div>
+
+<!-- wp:group {"align":"wide","layout":{"inherit":true}} -->
+				<div class="wp-block-group alignwide">
+
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Oops, we can’t seem to find the page you’re looking for.', 'shambhala-twentytwentytwo' ) . '</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>' . esc_html__( 'Error code: 404 Page not found.', 'shambhala-twentytwentytwo' ) . '</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Please use the links in the menu or the search form below.', 'shambhala-twentytwentytwo' ) . '</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":32} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+<!-- /wp:group -->
+
+<!-- wp:search {"label":"' . esc_html_x( 'Search', 'label', 'shambhala-twentytwentytwo' ) . '","showLabel":true,"width":90,"widthUnit":"%","buttonText":"' . esc_html__( 'Search', 'shambhala-twentytwentytwo' ) . '","buttonUseIcon":true,"align":"alignwide"} /-->
+
+
+<!-- wp:spacer {"height":32} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+',
 );
