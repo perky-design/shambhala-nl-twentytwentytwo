3b515bccbb4731c5e5f79f81262ea121582fc750
Joop Laan <joop.laan@gmail.com>
2022-07-12 07:39:00 +0200

    Fix php errors

--- functions/functions-events.php
+++ functions/functions-events.php
@@ -17,7 +17,10 @@ function shambhala_twentytwentytwo_filter_the_content( $content ) {
 		global $post;
 
 		// This is for the Events Calendar Plugin single page content.
-		if ( 'tribe_events' === $post->post_type && function_exists( 'tribe_get_event' ) ) {
+		if ( 'tribe_events' === $post->post_type
+			&& function_exists( 'tribe_get_event' )
+			&& function_exists( 'tribe_get_venue_details' )
+			&& function_exists( 'tribe_events_event_schedule_details' ) ) {
 			$pre_content   = '';
 			$post_content  = '';
 			$tribe_event   = tribe_get_event( $post->ID );
@@ -88,49 +91,22 @@ function shambhala_twentytwentytwo_get_registration_button( $tribe_meta ) {
  * @return string HTML of empty string.
  */
 function shambhala_twentytwentytwo_get_organizer( $event_id ) {
-	$organizer_id   = (int) tribe_get_organizer_id( $event_id );
-	$organizer_link = get_post_permalink( $organizer_id );
+	$html = '';
+	if ( function_exists( 'tribe_get_organizer_id' )
+		&& function_exists( 'tribe_get_organizer' ) ) {
+		$organizer_id   = (int) tribe_get_organizer_id( $event_id );
+		$organizer_link = get_post_permalink( $organizer_id );
 
-	$html .= '<h3>' . __( 'Organizer', 'shambhala-twentytwentytwo' ) . '</h3>';
-	$html .= '<h4>' . tribe_get_organizer( $post->ID ) . '</h4>';
-	$html .= '<p><a href="' . $organizer_link . '">' . __( 'View all events by this organizer', 'shambhala-twentytwentytwo' ) . '</a></p>';
+		$html .= '<h3>' . __( 'Organizer', 'shambhala-twentytwentytwo' ) . '</h3>';
+		$html .= '<h4>' . tribe_get_organizer( $event_id ) . '</h4>';
+		if ( is_string( $organizer_link ) ) {
+			/* translators: %s is replaced with "organizer link URL" */
+			$link = sprintf( wp_kses( __( '<a href="%s">View all events by this organizer</a>', 'shambhala-twentytwentytwo' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( $organizer_link ) );
+			$html .= '<p>' . $link . '</p>';
+		}
+	}
 
 	return $html;
-}
-
-/**
- * Display venue after content
- *
- * @param int $event_id The event ID..
- *
- * @return string HTML of empty string.
- */
-function shambhala_twentytwentytwo_get_venue( $event_id ) {
-	remove_filter( 'the_title', 'shambhala_twentytwentytwo_single_event_title' );
-
-	$venue_id      = tribe_get_venue_id( $event_id );
-	$venue_name    = wp_kses_post( get_the_title( $venue_id ) );
-	$venue_url     = tribe_get_venue_link( $event_id, false );
-	$venue_address = array(
-		'city'          => tribe_get_city( $event_id ),
-		'stateprovince' => tribe_get_stateprovince( $event_id ),
-		'zip'           => tribe_get_zip( $event_id ),
-		'street'        => tribe_get_address( $event_id ),
-	);
-
-	if ( $venue_name ) {
-
-		$html .= '<div class="tribe-events-venue-details">';
-		$html .= '<h2 class="tribe-events-single-section-title">' . esc_html__( 'Venue', 'shambhala-twentytwentytwo' ) . '</h2>';
-		$html .= '<h3>' . $venue_name . '</h3>';
-		$html .= '<p>' . $venue_address['street'] . '<br>';
-		$html .= $venue_address['zip'] . ' ' . $venue_address['city'] . '<br>';
-		$html .= '<a href="' . $venue_url . '">' . esc_html__( 'View venue calendar', 'shambhala-twentytwentytwo' ) . '</a>';
-		$html .= '</div><!-- tribe-events-venue-details -->';
-
-		return $html;
-	}
-	return '';
 }
 
 /**
@@ -140,49 +116,63 @@ function shambhala_twentytwentytwo_get_venue( $event_id ) {
  */
 function shambhala_twentytwentytwo_get_details() {
 
-	$event_id             = Tribe__Main::post_id_helper();
-	$time_format          = get_option( 'time_format', Tribe__Date_Utils::TIMEFORMAT );
-	$time_range_separator = tribe_get_option( 'timeRangeSeparator', ' - ' );
-	$show_time_zone       = tribe_get_option( 'tribe_events_timezones_show_zone', false );
-	$local_start_time     = tribe_get_start_date( $event_id, true, Tribe__Date_Utils::DBDATETIMEFORMAT );
-	$time_zone_label      = Tribe__Events__Timezones::is_mode( 'site' ) ? Tribe__Events__Timezones::wp_timezone_abbr( $local_start_time ) : Tribe__Events__Timezones::get_event_timezone_abbr( $event_id );
-	$start_datetime       = tribe_get_start_date();
-	$start_date           = tribe_get_start_date( null, false );
-	$start_time           = tribe_get_start_date( null, false, $time_format );
-	$start_ts             = tribe_get_start_date( null, false, Tribe__Date_Utils::DBDATEFORMAT );
-	$end_datetime         = tribe_get_end_date();
-	$end_date             = tribe_get_display_end_date( null, false );
-	$end_time             = tribe_get_end_date( null, false, $time_format );
-	$end_ts               = tribe_get_end_date( null, false, Tribe__Date_Utils::DBDATEFORMAT );
+	$event_id             = '';
+	$time_format          = '';
+	$time_range_separator = ' - ';
+	$show_time_zone       = false;
+	$local_start_time     = '';
+	$time_zone_label      = '';
+	$start_datetime       = '';
+	$start_date           = '';
+	$start_time           = '';
+	$start_ts             = '';
+	$end_datetime         = '';
+	$end_date             = '';
+	$end_time             = '';
+	$end_ts               = '';
+	$time_formatted       = '';
+	$time_title           = '';
 
-	$time_formatted = null;
-	if ( $start_time === $end_time ) {
-		$time_formatted = esc_html( $start_time );
-	} else {
-		$time_formatted = esc_html( $start_time . $time_range_separator . $end_time );
+	if ( class_exists( 'Tribe__Main' )
+		&& function_exists( 'tribe_events_get_event_website_title' )
+		&& function_exists( 'tribe_get_event_website_link' )
+		&& function_exists( 'tribe_get_formatted_cost' )
+		&& function_exists( 'tribe_get_end_date' )
+		&& function_exists( 'tribe_get_display_end_date' )
+		&& function_exists( 'tribe_get_start_date' )
+		&& function_exists( 'tribe_get_option' )
+		&& class_exists( 'Tribe__Date_Utils' )
+		&& class_exists( 'Tribe__Events__Timezones' ) ) {
+		$event_id             = Tribe__Main::post_id_helper();
+		$time_format          = get_option( 'time_format', Tribe__Date_Utils::TIMEFORMAT );
+		$time_range_separator = tribe_get_option( 'timeRangeSeparator', ' - ' );
+		$show_time_zone       = tribe_get_option( 'tribe_events_timezones_show_zone', false );
+		$local_start_time     = tribe_get_start_date( $event_id, true, Tribe__Date_Utils::DBDATETIMEFORMAT );
+		$time_zone_label      = Tribe__Events__Timezones::is_mode( 'site' ) ? Tribe__Events__Timezones::wp_timezone_abbr( $local_start_time ) : Tribe__Events__Timezones::get_event_timezone_abbr( $event_id );
+		$start_datetime       = tribe_get_start_date();
+		$start_date           = tribe_get_start_date( null, false );
+		$start_time           = tribe_get_start_date( null, false, $time_format );
+		$start_ts             = tribe_get_start_date( null, false, Tribe__Date_Utils::DBDATEFORMAT );
+		$end_datetime         = tribe_get_end_date();
+		$end_date             = tribe_get_display_end_date( null, false );
+		$end_time             = tribe_get_end_date( null, false, $time_format );
+		$end_ts               = tribe_get_end_date( null, false, Tribe__Date_Utils::DBDATEFORMAT );
+
+		$time_formatted = null;
+		if ( $start_time === $end_time ) {
+			$time_formatted = esc_html( $start_time );
+		} else {
+			$time_formatted = esc_html( $start_time . $time_range_separator . $end_time );
+		}
+
+		$time_formatted = apply_filters( 'tribe_events_single_event_time_formatted', $time_formatted, $event_id ); /* @phpstan-ignore-line */
+		$time_title     = apply_filters( 'tribe_events_single_event_time_title', __( 'Time:', 'shambhala-twentytwentytwo' ), $event_id ); /* @phpstan-ignore-line */
+		$cost           = tribe_get_formatted_cost();
+		$website        = tribe_get_event_website_link( $event_id );
+		$website_title  = tribe_events_get_event_website_title();
 	}
 
-	/**
-	 * Returns a formatted time for a single event
-	 *
-	 * @var string Formatted time string
-	 * @var int Event post id
-	 */
-	$time_formatted = apply_filters( 'tribe_events_single_event_time_formatted', $time_formatted, $event_id );
-
-	/**
-	 * Returns the title of the "Time" section of event details
-	 *
-	 * @var string Time title
-	 * @var int Event post id
-	 */
-	$time_title = apply_filters( 'tribe_events_single_event_time_title', __( 'Time:', 'shambhala-twentytwentytwo' ), $event_id );
-
-	$cost          = tribe_get_formatted_cost();
-	$website       = tribe_get_event_website_link( $event_id );
-	$website_title = tribe_events_get_event_website_title();
-
-	if ( isset( $start_datetime ) ) {
+	if ( isset( $start_datetime ) && function_exists( 'tribe_get_formatted_cost' ) ) {
 		$html = '';
 		$cost = tribe_get_formatted_cost();
 
@@ -197,72 +187,77 @@ function shambhala_twentytwentytwo_get_details() {
 		$html .= '<h2>' . esc_html__( 'Details', 'shambhala-twentytwentytwo' ) . '</h2>';
 		$html .= '<dl>';
 
-		// All day (multiday) events.
-		if ( tribe_event_is_all_day() && tribe_event_is_multiday() ) {
-			$html .= '<dt class="tribe-events-start-date-label">' . esc_html__( 'Start:', 'shambhala-twentytwentytwo' ) . '</dt>';
-			$html .= '<dd>';
-			$html .= '<abbr class="tribe-events-abbr tribe-events-start-date published dtstart" title="' . esc_attr( $start_ts ) . '">' . esc_html( $start_date ) . '</abbr>';
-			$html .= '</dd>';
-			$html .= '<dt class="tribe-events-end-date-label">' . esc_html__( 'End:', 'shambhala-twentytwentytwo' ) . '</dt>';
-			$html .= '<dd>';
-			$html .= '<abbr class="tribe-events-abbr tribe-events-end-date dtend" title="' . esc_attr( $end_ts ) . '">' . esc_html( $end_date ) . '</abbr>';
-			$html .= '</dd>';
-		} elseif ( tribe_event_is_all_day() ) {
-			// All day (single day) events.
-			$html .= '<dt class="tribe-events-start-date-label">' . esc_html__( 'Date:', 'shambhala-twentytwentytwo' ) . '</dt>';
-			$html .= '<dd>';
-			$html .= '<abbr class="tribe-events-abbr tribe-events-start-date published dtstart" title="' . esc_attr( $start_ts ) . '">' . esc_html( $start_date ) . '</abbr>';
-			$html .= '</dd>';
-		} elseif ( tribe_event_is_multiday() ) {
-			// Multiday events.
-			$html .= '<dt class="tribe-events-start-datetime-label">' . esc_html__( 'Start:', 'shambhala-twentytwentytwo' ) . '</dt>';
-			$html .= '<dd>';
-			$html .= '<abbr class="tribe-events-abbr tribe-events-start-datetime updated published dtstart" title="' . esc_attr( $start_ts ) . '">' . esc_html( $start_datetime ) . '</abbr>';
-			if ( $show_time_zone ) {
-				$html .= '<span class="tribe-events-abbr tribe-events-time-zone published ">' . esc_html( $time_zone_label ) . '</span>';
+		if ( function_exists( 'tribe_event_is_all_day' )
+			&& function_exists( 'tribe_event_is_multiday' ) ) {
+			// All day (multiday) events.
+			if ( tribe_event_is_all_day() && tribe_event_is_multiday() ) {
+				$html .= '<dt class="tribe-events-start-date-label">' . esc_html__( 'Start:', 'shambhala-twentytwentytwo' ) . '</dt>';
+				$html .= '<dd>';
+				$html .= '<abbr class="tribe-events-abbr tribe-events-start-date published dtstart" title="' . esc_attr( $start_ts ) . '">' . esc_html( $start_date ) . '</abbr>';
+				$html .= '</dd>';
+				$html .= '<dt class="tribe-events-end-date-label">' . esc_html__( 'End:', 'shambhala-twentytwentytwo' ) . '</dt>';
+				$html .= '<dd>';
+				$html .= '<abbr class="tribe-events-abbr tribe-events-end-date dtend" title="' . esc_attr( $end_ts ) . '">' . esc_html( $end_date ) . '</abbr>';
+				$html .= '</dd>';
+			} elseif ( tribe_event_is_all_day() ) {
+				// All day (single day) events.
+				$html .= '<dt class="tribe-events-start-date-label">' . esc_html__( 'Date:', 'shambhala-twentytwentytwo' ) . '</dt>';
+				$html .= '<dd>';
+				$html .= '<abbr class="tribe-events-abbr tribe-events-start-date published dtstart" title="' . esc_attr( $start_ts ) . '">' . esc_html( $start_date ) . '</abbr>';
+				$html .= '</dd>';
+			} elseif ( tribe_event_is_multiday() ) {
+				// Multiday events.
+				$html .= '<dt class="tribe-events-start-datetime-label">' . esc_html__( 'Start:', 'shambhala-twentytwentytwo' ) . '</dt>';
+				$html .= '<dd>';
+				$html .= '<abbr class="tribe-events-abbr tribe-events-start-datetime updated published dtstart" title="' . esc_attr( $start_ts ) . '">' . esc_html( $start_datetime ) . '</abbr>';
+				if ( $show_time_zone ) {
+					$html .= '<span class="tribe-events-abbr tribe-events-time-zone published ">' . esc_html( $time_zone_label ) . '</span>';
+				}
+				$html .= '</dd>';
+				$html .= '<dt class="tribe-events-end-datetime-label">' . esc_html__( 'End:', 'shambhala-twentytwentytwo' ) . '</dt>';
+				$html .= '<dd>';
+				$html .= '<abbr class="tribe-events-abbr tribe-events-end-datetime dtend" title="' . esc_attr( $end_ts ) . '">' . esc_html( $end_datetime ) . '</abbr>';
+				if ( $show_time_zone ) {
+					$html .= '<span class="tribe-events-abbr tribe-events-time-zone published ">' . esc_html( $time_zone_label ) . '</span>';
+				}
+				$html .= '</dd>';
+			} else {
+				// Single day events.
+				$html .= '<dt class="tribe-events-start-date-label">' . esc_html__( 'Date:', 'shambhala-twentytwentytwo' ) . '</dt>';
+				$html .= '<dd>';
+				$html .= '<abbr class="tribe-events-abbr tribe-events-start-date published dtstart" title="' . esc_attr( $start_ts ) . '">' . esc_html( $start_date ) . '</abbr>';
+				$html .= '</dd>';
+				$html .= '<dt class="tribe-events-start-time-label">' . esc_html( $time_title ) . '</dt>';
+				$html .= '<dd>';
+				$html .= '<div class="tribe-events-abbr tribe-events-start-time published dtstart" title="' . esc_attr( $end_ts ) . '">';
+				$html .= $time_formatted;
+				if ( $show_time_zone ) {
+					$html .= '<span class="tribe-events-abbr tribe-events-time-zone published ">' . esc_html( $time_zone_label ) . '</span>';
+				}
+				$html .= '</div></dd>';
 			}
-			$html .= '</dd>';
-			$html .= '<dt class="tribe-events-end-datetime-label">' . esc_html__( 'End:', 'shambhala-twentytwentytwo' ) . '</dt>';
-			$html .= '<dd>';
-			$html .= '<abbr class="tribe-events-abbr tribe-events-end-datetime dtend" title="' . esc_attr( $end_ts ) . '">' . esc_html( $end_datetime ) . '</abbr>';
-			if ( $show_time_zone ) {
-				$html .= '<span class="tribe-events-abbr tribe-events-time-zone published ">' . esc_html( $time_zone_label ) . '</span>';
+
+			if ( $cost ) {
+				$html .= '<dt class="tribe-events-event-cost-label">' . esc_html__( 'Cost:', 'shambhala-twentytwentytwo' ) . '</dt>';
+				$html .= '<dd class="tribe-events-event-cost">' . esc_attr( $cost ) . '</dd>';
 			}
-			$html .= '</dd>';
-		} else {
-			// Single day events.
-			$html .= '<dt class="tribe-events-start-date-label">' . esc_html__( 'Date:', 'shambhala-twentytwentytwo' ) . '</dt>';
-			$html .= '<dd>';
-			$html .= '<abbr class="tribe-events-abbr tribe-events-start-date published dtstart" title="' . esc_attr( $start_ts ) . '">' . esc_html( $start_date ) . '</abbr>';
-			$html .= '</dd>';
-			$html .= '<dt class="tribe-events-start-time-label">' . esc_html( $time_title ) . '</dt>';
-			$html .= '<dd>';
-			$html .= '<div class="tribe-events-abbr tribe-events-start-time published dtstart" title="' . esc_attr( $end_ts ) . '">';
-			$html .= $time_formatted;
-			if ( $show_time_zone ) {
-				$html .= '<span class="tribe-events-abbr tribe-events-time-zone published ">' . esc_html( $time_zone_label ) . '</span>';
-			}
-			$html .= '</div></dd>';
 		}
 
-		if ( $cost ) {
-			$html .= '<dt class="tribe-events-event-cost-label">' . esc_html__( 'Cost:', 'shambhala-twentytwentytwo' ) . '</dt>';
-			$html .= '<dd class="tribe-events-event-cost">' . esc_attr( $cost ) . '</dd>';
+		if ( function_exists( 'tribe_get_event_categories' ) ) {
+			$html .= tribe_get_event_categories(
+				get_the_id(),
+				array(
+					'before'       => '',
+					'sep'          => ', ',
+					'after'        => '',
+					'label'        => null, // An appropriate plural/singular label will be provided.
+					'label_before' => '<dt class="tribe-events-event-categories-label">',
+					'label_after'  => '</dt>',
+					'wrap_before'  => '<dd class="tribe-events-event-categories">',
+					'wrap_after'   => '</dd>',
+				)
+			);
 		}
-
-		$html .= tribe_get_event_categories(
-			get_the_id(),
-			array(
-				'before'       => '',
-				'sep'          => ', ',
-				'after'        => '',
-				'label'        => null, // An appropriate plural/singular label will be provided.
-				'label_before' => '<dt class="tribe-events-event-categories-label">',
-				'label_after'  => '</dt>',
-				'wrap_before'  => '<dd class="tribe-events-event-categories">',
-				'wrap_after'   => '</dd>',
-			)
-		);
 
 		$html .= '</dl></div><!-- tribe-events-meta-group tribe-events-meta-group-details -->';
 		$html .= '</div><!-- /wp:column --></div><!-- /wp:columns --></div><!-- /wp:group -->';
